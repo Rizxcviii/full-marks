1f6261356b35a9b7a81692ed03c44306607a9850
hhh99-hub <55915464+hhh99-hub@users.noreply.github.com>
2020-04-05 15:32:37 +0100

    Added Examiner_login Page
    Added Examiner_login.html

--- FrontEnd/process-form.php
+++ FrontEnd/process-form.php
@@ -8,10 +8,10 @@
     <h1>Thank You</h1>
     <p>Here is the information you have submitted:</p>
     <ol>
-        <li><em>Name:</em> <?php echo $_POST["name"]?></li>
+        <li><em>Name:</em> <?php echo $_POST["username"]?></li>
         <li><em>Email:</em> <?php echo $_POST["email"]?></li>
-        <li><em>Subject:</em> <?php echo $_POST["subject"]?></li>
-        <li><em>Message:</em> <?php echo $_POST["message"]?></li>
+        <li><em>Subject:</em> <?php echo $_POST["password"]?></li>
+        
     </ol>
 </body>
 </html>
