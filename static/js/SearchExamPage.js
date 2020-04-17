d82bbe4ddedd732f1b537a276c5c87162b90694a
Rizbir Fahmid Khan <ec18115@qmul.ac.uk>
2020-04-17 17:00:36 +0100

    added searching of exams for registered/non-registered students

--- static/js/SearchExamPage.js
+++ static/js/SearchExamPage.js
@@ -1,40 +1,48 @@
 let submit = document.getElementById('submit');
 let searchBar = document.getElementById('searchBar');
 let search = document.getElementById('search');
+let sid = document.getElementById('sid');
 let exam;
-$('.response').hide();
-$('#submit').hide();
+$('.response, #submit, #sid').hide();
+('#sid')
 
 async function searchForExam(){
     if(searchBar.value == ""){
         $('.response').hide();
         $('.response').html("<p>Please enter a module code/ID</p>");
         $('.response').fadeIn(300);
-        $('#submit').hide();
+        $('#submit, #sid').hide();
         return;
     }
     response = await networkController.sendDataToBackend({searched:searchBar.value,startExam:false}, '/searchExam');
     if(response.message == 'exam does not exist'){
-        $('.response').hide();
+        $('.response, #submit, #sid').hide();
         $('.response').html("<p>"+response.message+"</p>");
         $('.response').fadeIn(300);
-        $('#submit').hide();
     }else{
         console.log(response);
-        $('.response').hide();
-        $('#submit').hide();
+        $('.response, #submit, #sid').hide();
         if (response.loggedIn == true) {
-            $('.response').html("<p>"+response.exam.markScheme.examName+" has been found. Please press submit to take the exam</p>");
+            $('.response').html("<p>"+response.exam.examName+" has been found. Please press submit to take the exam</p>");
         } else {
-            $('.response').html("<p>"+response.exam.markScheme.examName+" has been found. However it seems you are not registered with full marks, so please enter your Student ID where stated</p>");
+            $('.response').html("<p>"+response.exam.examName+" has been found. However it seems you are not registered with full marks, so please enter your Student ID where stated and press submit</p>");
+            $('#sid').fadeIn(300);
         }
-        $('.response').fadeIn(300);
-        $('#submit').fadeIn(300);
+        $('.response, #submit').fadeIn(300);
         exam = searchBar.value;
     }
 };
 
 async function startExam(){
-    await networkController.sendDataToBackend({examCode:exam,startExam:true}, '/searchExam');
+    if ($('#sid').is(':visible') && $.trim($('#sid').val()).length == 0) {
+        $('.response, #submit, #sid').hide();
+        $('.response').html("<p>The student ID is empty, please enter your student ID</p>");
+        $('.response').fadeIn(300);
+        return;
+    }else if ($('#sid').is(':visible')) {
+        response = await networkController.sendDataToBackend({examCode:exam, sid:sid.value, startExam:true}, '/searchExam');
+    }else{
+        response = await networkController.sendDataToBackend({examCode:exam, startExam:true}, '/searchExam');
+    }
     networkController.redirect('tAndC');
 }
