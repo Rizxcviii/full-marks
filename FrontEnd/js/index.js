9edd5097847064809a276f4300ba8612c6607630
Hemang Huria <h.huria@se18.qmul.ac.uk>
2020-04-19 17:26:22 +0100

    Updated the Register&Login.js Page
    Updated the Register&Login Page in frontEnd and static folder

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,13 +1,12 @@
-$(window).on("hashchange", function () {
-  if (location.hash.slice(1) == "login") {
-    $(".card").addClass("extend");
-
-    $("#register").removeClass("selected");
-    $("#login").addClass("selected");
-  } else {
-    $(".card").removeClass("extend");
-    $("register").addClass("selected");
-    $("#login").removeClass("selected");
-  }
+$(window).on("hashchange", function(){
+	if(location.hash.slice(1)=="login"){
+		$(".card").addClass("extend");
+		$("#register").removeClass("selected");
+		$("#login").addClass("selected");
+	} else {
+		$(".card").removeClass("extend");
+		$("#register").addClass("selected");
+		$("#login").removeClass("selected");
+	}
 });
 $(window).trigger("hashchange");
